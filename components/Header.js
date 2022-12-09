f70e6166a4676bcff6bbe2ed615716b5d33a322e
Christian <sefluntok@gmail.com>
2022-12-09 22:55:53 +1100

    Added Primary Button. Tested on Header Component.

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,20 @@ import PrimaryButton from './Button/PrimaryButton'
 
 const Header = () => {
     return (
-        <div className="text-3xl font-bold underline my-12 wrap">
-            <h2>Header</h2>
+        <div>
+            <div className="text-3xl font-bold underline my-12 wrap">
+                <h2>Header</h2>
+            </div>
             <Icon icon="material-symbols:arrow-right-alt" />
-            <PrimaryButton />
+            <PrimaryButton 
+                     text="Get Started"
+                     link="/"
+            />
+            <PrimaryButton 
+                     text="About"
+                     link="/"
+                     icon="material-symbols:arrow-right-alt"
+            />
         </div>
     )
 }
