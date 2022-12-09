9d6a0033ce9847ad2ac011e316e581e61241020b
Christian <sefluntok@gmail.com>
2022-12-09 22:57:43 +1100

    Updated Tailwind config updates to extend.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,37 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    fontFamily: {
-      primary: ['Poppins'],
-      secondary: ['"Open Sans"'],
-    }, colors: {
-      primary: {
-        50: "#EDEDF8",
-        100: "#DBDBF0",
-        200: "#B7B7E1",
-        300: "#9392D3",
-        400: "#6F6EC4",
-        500: "#4E4CB6",
-        600: "#3C3B91",
-        700: "#2D2C6D",
-        800: "#1E1E48",
-        900: "#0F0F24"
+    extend: {
+      fontFamily: {
+        primary: ['Inter'],
+        secondary: ['"Open Sans"'],
+      }, colors: {
+        primary: {
+          50: "#EDEDF8",
+          100: "#DBDBF0",
+          200: "#B7B7E1",
+          300: "#9392D3",
+          400: "#6F6EC4",
+          500: "#4E4CB6",
+          600: "#3C3B91",
+          700: "#2D2C6D",
+          800: "#1E1E48",
+          900: "#0F0F24"
+        }, 
+        secondary: {
+          50: "#EAEDF1",
+          100: "#D7DDE4",
+          200: "#B0BBC9",
+          300: "#8898AF",
+          400: "#637692",
+          500: "#475569",
+          600: "#3A4555",
+          700: "#2B3440",
+          800: "#1D232B",
+          900: "#0E1115"
+        }
       }
-    }
+    },
   },
   plugins: [],
 }
