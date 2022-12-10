aa2852f1a25d3feebeb9efc22dcf063a68284749
Christian <sefluntok@gmail.com>
2022-12-10 15:20:29 +1100

    Responsive Navigation functionality complete.

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
-import { Icon } from '@iconify/react'
 import PrimaryButton from '@components/Button/PrimaryButton'
 import SecondaryButton from '@components/Button/SecondaryButton'
+import Nav from '@components/Nav'
 
 const Header = () => {
     return (
         <header id="header" className="wrap wrap-px">
-            <div className="header-container py-4 flex items-center">
-                <div className="flex items-center mr-6">
-                    <a href="/" className="">
+            <div className="header-container relative py-4 justify-between md:justify-start flex items-center">
+                <div className="flex items-center mr-8">
+                    <a href="/" className="logo">
                         <img 
                             src="/icons/Pathway.svg" 
                             alt="logo"
@@ -15,20 +15,15 @@ const Header = () => {
                         />
                     </a>
                 </div>
-                <nav className="header-nav text-black/60">
-                    <ul className="flex font-medium gap-6">
-                        <li><a href="">Use cases</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="">Company</a></li>
-                        <li><a href="#">About</a></li>
-                    </ul>
-                </nav>
-                <div className="button-container flex flex-wrap gap-4 ml-auto">
+                <Nav />
+                <div className="button-container hidden md:flex flex-wrap gap-4 ml-auto">
                     <SecondaryButton 
                         text="Login"
+                        link="/login"
                     />
                     <PrimaryButton 
                         text="Sign Up"
+                        link="/sign-up"
                     />
                 </div>
             </div>
