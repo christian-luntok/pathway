d60e1ba9fc797c378af473d6e9a4f5991ed239ad
Christian <sefluntok@gmail.com>
2022-12-11 10:50:20 +1100

    Removed Button Group on Section content. Added new component button group on Hero component.

--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,32 @@
 import SectionContent from "@components/Section/SectionContent"
+import ButtonGroup from "@components/ButtonGroup"
 import SectionImage from "@components/Section/SectionImage"
+
 
 const Hero = () => {
 
     return (
         <section className="hero-container">
-            <SectionContent 
-                title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                content = "Fusce ac nulla non urna posuere malesuada eget id ipsum. Praesent vitae sem nisl. Donec in est ut odio sagittis dapibus in commodo mi."
-                subheading = "Sub Heading"
-                alignment = "center"
-                isBigHeader = {true}
-            />
+            <div className="hero-content-container py-16 ">
+                <SectionContent 
+                    title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    content = "Fusce ac nulla non urna posuere malesuada eget id ipsum. Praesent vitae sem nisl. Donec in est ut odio sagittis dapibus in commodo mi."
+                    subheading = "Sub Heading"
+                    alignment = "center"
+                    isBigHeader = {true}
+                />
+                <ButtonGroup 
+                    wrap = "wrap-sm wrap-px"
+                    hasPrimary = {true}
+                    primaryText = "About"
+                    primaryLink = "/about"
+                    primaryIcon = "ph:arrow-right-bold"
+                    hasSecondary = {true}
+                    secondaryText = "Company"
+                    secondaryLink = "/company"
+                    alignment="center"
+                />
+            </div>
             <SectionImage 
                 src="images/Hero.jpg"
                 alt="Hero Image used for Hero Banner"
--- components/Section/SectionContent.js
+++ components/Section/SectionContent.js
@@ -1,18 +1,15 @@
 import SectionTitle from "@components/Section/SectionTitle"
-import PrimaryButton from '@components/Button/PrimaryButton'
-import SecondaryButton from '@components/Button/SecondaryButton'
-
 // You can get icons from iconify - just add the name on the Primary Button Props
 
 
 const SectionContent = ({content, title, subheading, alignment, isBigHeader }) => {
     const alignClass = alignment === 'center' 
         ? 'text-center justify-center m-auto' : alignment === 'left' 
-        ? 'text-left justify-start ml-auto' : alignment === 'right' 
-        ? 'text-right justify-end mr-auto' : null;
+        ? 'text-left justify-start mr-auto' : alignment === 'right' 
+        ? 'text-right justify-end ml-auto' : null;
     return (
         <section className = {`section-content-container wrap wrap-px `}>
-            <div className={`section-content py-16 ${ alignClass ? alignClass : null }`}>
+            <div className={`section-content ${ alignClass ? alignClass : null }`}>
                 <SectionTitle 
                     title = {title}
                     subheading = {subheading}
@@ -20,17 +17,6 @@ const SectionContent = ({content, title, subheading, alignment, isBigHeader }) =
                 />
                 <div className="content-container text-black/60">
                     <p>{ content }</p>
-                </div>
-                <div className={`button-container gap-4 flex flex-wrap ${ alignClass ? alignClass : null }`}>
-                    <SecondaryButton 
-                        text="About"
-                        link="/about"
-                        icon="ph:arrow-right-bold"
-                    />
-                    <PrimaryButton 
-                        text="Company"
-                        link="/company"
-                    />
                 </div>
             </div>
         </section>
