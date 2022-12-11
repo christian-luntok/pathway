4ee3fee492e35871795aa35cd3b6ed5350827396
Christian <sefluntok@gmail.com>
2022-12-11 17:06:45 +1100

    Added Highlight on index page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from "@components/Hero"
+import Highlight from "@components/Highlight"
 
 const Home = () => {
   return (
     <section className="">
         <Hero />
+        <Highlight />
     </section>
   )
 }
