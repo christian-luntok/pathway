9bef63a64be3d519c7447915f0273255e44ceeb6
Christian <sefluntok@gmail.com>
2022-12-11 21:09:19 +1100

    Added reverse display for grid.

--- components/Section/SectionHighlight.js
+++ components/Section/SectionHighlight.js
@@ -1,34 +1,71 @@
 import SectionIconContent from "@components/Section/SectionIconContent"
 import ButtonGroup from "@components/ButtonGroup"
 
-const SectionHighlight = ({ title, content, isReversed, icon, image, imageAlt, link}) => {
-
+const SectionHighlight = ({ title, content, isReversed, icon, image, imageAlt, link, linkTitle }) => {
     return (
         <section className="highlight-content-container wrap wrap-px">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-                <div className="image-container">
-                    <figure className="w-[512px]">
-                        <img 
-                            src="images/highlight-1.jpg"
-                            alt={imageAlt}
-                        />
-                    </figure>
-                </div>
-                <div className="content-container w-[512px] ml-auto">
-                    <SectionIconContent 
-                        title = {title}
-                        content = {content}
-                        icon = {icon}
-                        alignment = "left"
-                    />
-                    <ButtonGroup 
-                        wrap = "wrap-sm"
-                        hasPrimary = {true}
-                        primaryText = "About"
-                        primaryLink = "/about"
-                    />
-                </div>
-            </div>
+                {
+                    isReversed ? (
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center mb-16">
+                            <div className="content-container w-[512px] mr-auto">
+                                <SectionIconContent 
+                                    title = {title}
+                                    content = {content}
+                                    icon = {icon}
+                                    alignment = "left"
+                                />
+                                { 
+                                    link ? (
+                                        <ButtonGroup 
+                                        wrap = "wrap-sm"
+                                        hasPrimary = {true}
+                                        primaryText = {linkTitle}
+                                        primaryLink = {link}
+                                        />
+                                    ) : null
+                                }
+                            </div>
+                            <div className="image-container">
+                                <figure className="w-[512px]">
+                                    <img 
+                                        src={image}
+                                        alt={imageAlt}
+                                        className="ml-auto"
+                                    />
+                                </figure>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center  mb-16">
+                            <div className="image-container">
+                                <figure className="w-[512px]">
+                                    <img 
+                                        src={image}
+                                        alt={imageAlt}
+                                    />
+                                </figure>
+                            </div>
+                            <div className="content-container w-[512px] ml-auto">
+                                <SectionIconContent 
+                                    title = {title}
+                                    content = {content}
+                                    icon = {icon}
+                                    alignment = "left"
+                                />
+                                { 
+                                    link ? (
+                                        <ButtonGroup 
+                                        wrap = "wrap-sm"
+                                        hasPrimary = {true}
+                                        primaryText = {linkTitle}
+                                        primaryLink = {link}
+                                        />
+                                    ) : null
+                                }
+                            </div>
+                        </div>
+                    )
+                }
         </section>
     )
 }
