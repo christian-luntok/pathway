fdabf0fdbebd13033b0c3cbb55c29155be97a5ea
Christian <sefluntok@gmail.com>
2022-12-11 14:17:19 +1100

    Changed from button to Next Link.

--- components/Button/PrimaryButton.js
+++ components/Button/PrimaryButton.js
@@ -1,22 +1,22 @@
 import { Icon } from '@iconify/react'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 // You can get icons from iconify - just add the name on the Primary Button Props
 const PrimaryButton = ({text, icon, link}) => {
-    const router = useRouter()
     return (
-        <button
-            onClick={() => router.push(link)}
-            className="flex font-medium justify-center gap-3 items-center px-8 bg-primary-500 text-white py-[5px] rounded-full hover:bg-primary-300 transition-all duration-300"
+        <Link
+            href = { link }
+            role ="button"
+            className="flex font-medium w-full md:w-auto justify-center gap-3 items-center px-8 bg-primary-500 text-white py-[5px] rounded-full hover:bg-primary-300 transition-all duration-300"
         >
-            {text}
+            { text }
             {
                     icon?.length > 0 
                     ? (
                         <Icon icon={icon}/>    
                     ) : null
             }
-        </button>
+        </Link>
     )
 }
 
--- components/Button/SecondaryButton.js
+++ components/Button/SecondaryButton.js
@@ -1,21 +1,21 @@
 import { Icon } from '@iconify/react'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const PrimaryButton = ({text, icon, link}) => {
-    const router = useRouter()
     return (
-        <button
-            onClick={() => router.push(link)}
-            className="flex font-medium justify-center gap-3 items-center px-8 bg-secondary-500 text-white py-[5px] rounded-full hover:bg-secondary-300 transition-all duration-300"
+        <Link
+            href = { link }
+            role ="button"
+            className="flex font-medium w-full md:w-auto justify-center gap-3 items-center px-8 bg-secondary-500 text-white py-[5px] rounded-full hover:bg-secondary-300 transition-all duration-300"
         >
-            {text}
+            { text }
             {
                     icon?.length > 0 
                     ? (
                         <Icon icon={icon}/>    
                     ) : null
             }
-        </button>
+        </Link>
     )
 }
 
