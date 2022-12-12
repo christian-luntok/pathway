2c2d7d5fa5f00a25948a77eae502f6ddc472aed3
Christian <sefluntok@gmail.com>
2022-12-13 10:19:52 +1100

    Added conditional statement to change the element based on {type}.

--- components/Button/PrimaryButton.js
+++ components/Button/PrimaryButton.js
@@ -2,9 +2,10 @@ import { Icon } from '@iconify/react'
 import Link from 'next/link'
 
 // You can get icons from iconify - just add the name on the Primary Button Props
-const PrimaryButton = ({text, icon, link}) => {
+const PrimaryButton = ({text, icon, link, type}) => {
+    const Element = type === 'button' ? 'button' : Link
     return (
-        <Link
+        <Element
             href = { link }
             role ="button"
             className="flex font-medium w-full md:w-auto justify-center gap-3 items-center px-8 bg-primary-500 text-white py-2 rounded-full hover:bg-primary-300 transition-all duration-300"
@@ -16,8 +17,9 @@ const PrimaryButton = ({text, icon, link}) => {
                         <Icon icon={icon}/>    
                     ) : null
             }
-        </Link>
+        </Element>
     )
 }
 
 export default PrimaryButton
+
