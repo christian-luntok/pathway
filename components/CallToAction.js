91c1df63d224020a3f9c4deb04db8bebeb6e98bc
Christian <sefluntok@gmail.com>
2022-12-12 21:54:47 +1100

    Initialize Call to action component.

---
+++ components/CallToAction.js
@@ -0,0 +1,22 @@
+import SectionContent from '@components/Section/SectionContent'
+import SubscribeForm from '@components/Forms/SubscribeForm'
+
+const CallToAction = () => {
+
+    return (
+        <section className="cta-container">
+            <div className="cta-content-container wrap wrap-px py-24">
+                <SectionContent 
+                    title = "Lorem 1,000+ Ipsum Title"
+                    content = "Duis malesuada faucibus est. Ut pulvinar gravida sapien sed efficitur."
+                    alignment = "center"
+                />
+                <SubscribeForm
+                    alignment = "center"
+                />
+            </div>
+        </section>
+    )
+}
+
+export default CallToAction
