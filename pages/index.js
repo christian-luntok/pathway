689a1858f5137e80dead93979c12104ea92707e7
Christian <sefluntok@gmail.com>
2022-12-12 21:54:58 +1100

    Added cta component on index homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Hero from "@components/Hero"
 import Highlight from "@components/Highlight"
 import Feature from "@components/Feature"
+import CTA from "@components/CallToAction"
+
 const Home = () => {
   return (
     <section className="">
         <Hero />
         <Highlight />
         <Feature />
+        <CTA />
     </section>
   )
 }
