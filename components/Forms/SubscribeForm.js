b3f751b92225c0aa0b686a2811059f0cc0b44407
Christian <sefluntok@gmail.com>
2022-12-13 11:24:14 +1100

    Added responsive classes for subscribe component.

--- components/Forms/SubscribeForm.js
+++ components/Forms/SubscribeForm.js
@@ -16,8 +16,8 @@ const SubscribeForm = ({alignment}) => {
 
     return(
         <div className="form-container">
-            <form className={`flex flex-wrap gap-4 ${ alignClass ? alignClass : '' }`} onSubmit={handleSubmit}>
-                <div className="relative rounded-full max-w-[320px] w-full border-2 border-gray-300 text-black/60 py-2 px-4">
+            <form className={`flex flex-wrap gap-4 text-center justify-center ${ alignClass ? alignClass : '' }`} onSubmit={handleSubmit}>
+                <div className="relative rounded-full md:max-w-[320px] w-full border-2 border-gray-300 text-black/60 py-2 px-4">
                     <input 
                         className="form-input placeholder-black/60 bg-transparent w-full pl-8 focus-visible:border-0 focus-visible:outline-none" 
                         id="email"
@@ -30,8 +30,8 @@ const SubscribeForm = ({alignment}) => {
                         <Icon className="h-5 w-5 text-black/60" icon="ic:outline-email"/>
                     </div>
                 </div>
-                <div className="form-input">
-                    <PrimaryButton text="Submit" link="/" type="button"/>
+                <div className="form-input w-full md:w-auto">
+                    <PrimaryButton className="w-full md:w-auto" text="Submit" link="/" type="button"/>
                 </div>
             </form>
         </div>
