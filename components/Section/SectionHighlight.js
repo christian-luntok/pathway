df35202e1bdac96abfee41056103530b9c3a7188
Christian <sefluntok@gmail.com>
2022-12-13 21:37:45 +1100

    Added conditional statements for image in Section Highlight component.

--- components/Section/SectionHighlight.js
+++ components/Section/SectionHighlight.js
@@ -7,16 +7,20 @@ const SectionHighlight = ({ title, content, isReversed, icon, image, imageAlt, l
         <section className="highlight-content-container wrap wrap-px">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mb-16">
                 <div className={`image-container ${ isReversed ? 'order-1 md:order-2' : 'order-1' }`}>
-                    <figure className="w-[512px]">
-                        <Image 
-                             priority
-                             src={image}
-                             alt={imageAlt}
-                             className={isReversed ? 'ml-auto' : ''}
-                             width={515}
-                             height={515}
-                        />
-                    </figure>
+                        {
+                            image ? (
+                                <figure className="w-[512px]">
+                                    <Image 
+                                        priority
+                                        src={image}
+                                        alt={imageAlt}
+                                        className={isReversed ? 'ml-auto' : ''}
+                                        width={515}
+                                        height={515}
+                                    />
+                                </figure>
+                            ) : null
+                        }
                 </div>
                 <div className={`content-container w-[512px] ${ isReversed ? 'order-2 md:order-1 mr-auto' : 'order-2 ml-auto' }`}>
                     <SectionIconContent 
