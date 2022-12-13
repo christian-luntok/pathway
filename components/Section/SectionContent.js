a34176d6db296b5eee0a1007cca9d959e49191e7
Christian <sefluntok@gmail.com>
2022-12-13 21:37:05 +1100

    Added component padding for section content.

--- components/Section/SectionContent.js
+++ components/Section/SectionContent.js
@@ -2,13 +2,13 @@ import SectionTitle from "@components/Section/SectionTitle"
 // You can get icons from iconify - just add the name on the Primary Button Props
 
 
-const SectionContent = ({content, title, subheading, alignment, isBigHeader }) => {
+const SectionContent = ({content, title, subheading, alignment, isBigHeader, hasBlockPadding }) => {
     const alignClass = alignment === 'center' 
         ? 'md:text-center md:justify-center m-auto' : alignment === 'left' 
         ? 'md:text-left md:justify-start mr-auto' : alignment === 'right' 
         ? 'md:text-right md:justify-end ml-auto' : null;
     return (
-        <section className = {`section-content-container wrap wrap-px `}>
+        <section className = {`section-content-container wrap wrap-px ${hasBlockPadding ? 'py-6 sm:py-24' : '' }`}>
             <div className={`section-content text-center ${ alignClass ? alignClass : '' }`}>
                 <SectionTitle 
                     title = {title}
