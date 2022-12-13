a69cdc01635601f9b94d536032086305231f1675
Christian <sefluntok@gmail.com>
2022-12-13 21:36:12 +1100

    Added Page Title component.

---
+++ components/PageTitle.js
@@ -0,0 +1,10 @@
+
+const PageTitle = ({text}) => {
+    return (
+        <div className="page-title-container text-center py-6 sm:py-12">
+            <h1 className="font-primary text-3xl sm:text-4xl font-extrabold">{text}</h1>
+        </div>
+    )
+}
+
+export default PageTitle
