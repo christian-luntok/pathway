aef91aee37fccadb081b1a8f6c7505da9d54826e
Christian <sefluntok@gmail.com>
2022-12-13 20:28:15 +1100

    Added Logo Items component.

---
+++ components/Logos/LogoItems.js
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+const LogoItems = ({logos}) => {
+
+    return (
+        <div className="logos-container flex flex-wrap md:grid align-center justify-center gap-y-6 gap-x-8 md:gap-x-14 md:grid-cols-4 xl:grid-cols-8">
+            {
+                logos.map((item,index) => (
+                    <div key={index} className="logo-item-container flex align-center flex-wrap m-auto md:col-span-1">
+                        <Image 
+                            src={item.logo}
+                            width="110"
+                            height="25"
+                        />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default LogoItems
