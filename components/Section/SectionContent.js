9bb9a38de2e3cb46c625a51f0d5d3a6a82f025e4
Christian <sefluntok@gmail.com>
2022-12-13 22:06:45 +1100

    Moved block padding on inner container.

--- components/Section/SectionContent.js
+++ components/Section/SectionContent.js
@@ -2,18 +2,19 @@ import SectionTitle from "@components/Section/SectionTitle"
 // You can get icons from iconify - just add the name on the Primary Button Props
 
 
-const SectionContent = ({content, title, subheading, alignment, isBigHeader, hasBlockPadding }) => {
+const SectionContent = ({content, title, subheading, alignment, isBigHeader, hasBlockPadding, icon }) => {
     const alignClass = alignment === 'center' 
         ? 'md:text-center md:justify-center m-auto' : alignment === 'left' 
         ? 'md:text-left md:justify-start mr-auto' : alignment === 'right' 
         ? 'md:text-right md:justify-end ml-auto' : null;
     return (
-        <section className = {`section-content-container wrap wrap-px ${hasBlockPadding ? 'py-6 sm:py-24' : '' }`}>
-            <div className={`section-content text-center ${ alignClass ? alignClass : '' }`}>
+        <section className = {`section-content-container wrap wrap-px`}>
+            <div className={`section-content text-center ${hasBlockPadding ? 'my-6 sm:my-24' : '' } ${ alignClass ? alignClass : '' }`}>
                 <SectionTitle 
                     title = {title}
                     subheading = {subheading}
                     isBigHeader = {isBigHeader}
+                    icon = {icon}
                 />
                 <div className="content-container text-black/60">
                     <p>{ content }</p>
