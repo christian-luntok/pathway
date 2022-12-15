128e1ee8d8f2956b479314510efcf315bfbc4394
Christian <sefluntok@gmail.com>
2022-12-16 10:38:31 +1100

    Updated button to make use of variants.
    Co-authored-by: Joshua Germon <joshgermon@gmail.com>

---
+++ components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
+
+export enum ButtonVariant {
+    primary = 'bg-primary-500',
+    secondary ='bg-secondary-500'
+}
+
+type ButtonProps = {
+    text: string
+    icon?: string
+    link: string
+    variant?: ButtonVariant
+    type?: string | 'button' | 'link'
+  }
+  
+// You can get icons from iconify - just add the name on the Primary Button Props
+const Button = ({ text, icon, link, type = 'link', variant }: ButtonProps ) => {
+    const Element = type === 'button' ? 'button' : Link
+    return (
+        <Element
+            href = { link }
+            role ="button"
+            className={`flex font-medium w-full md:w-auto justify-center gap-3 items-center px-8  text-white py-2 rounded-full hover:bg-primary-300 transition-all duration-300 ${variant && variant}`}
+        >
+            { text }
+            {
+                    icon?.length && (
+                        <Icon icon={icon}/>    
+                    ) 
+            }
+        </Element>
+    )
+}
+
+export default Button
+
--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -1,5 +1,4 @@
-import PrimaryButton from '@components/Button/PrimaryButton'
-import SecondaryButton from '@components/Button/SecondaryButton'
+import PrimaryButton from '@components/Button/Button'
 
 /** 
  * Button Group Component
@@ -12,7 +11,7 @@ import SecondaryButton from '@components/Button/SecondaryButton'
  * @params alignment - lets you choose to align your button group to [left, right, center]
  * 
  * <ButtonGroup 
-      wrap = "wrap-sm wrap-px" - 
+        wrap = "wrap-sm wrap-px" - 
         hasPrimary = {true} - 
         primaryText = "About" - 
         primaryLink = "/about"
@@ -24,38 +23,86 @@ import SecondaryButton from '@components/Button/SecondaryButton'
     />
  */
 
-const ButtonGroup = ({wrap, alignment, hasPrimary, hasSecondary, primaryText, primaryLink, primaryIcon, primaryType, secondaryText, secondaryLink, secondaryIcon, secondaryType}) => {
+// own dataset something like that - yeah your own type as in it has to be one of the strings in the enum?
+
+// so you are creating your own type i guess
+
+type ButtonGroupProps = {
+    className?: string
+    hasPrimary?: boolean
+    hasSecondary?: boolean
+    alignment?: string
+}
+
+const ButtonGroup = ({className, alignment, children}: any) => {
     const alignClass = alignment === 'center' 
         ? 'justify-center m-auto' : alignment === 'left' 
         ? 'justify-start mr-auto' : alignment === 'right' 
         ? 'justify-end ml-auto' : null;
     return ( 
         <div className="button-container">
-            <div className={`buttons ${wrap} gap-4 mt-6 flex flex-wrap ${ alignClass && alignClass}`}>
-                {
-                    hasPrimary && (
-                        <PrimaryButton 
-                            text={ primaryText && primaryText }
-                            link={ primaryLink && primaryLink }
-                            icon={ primaryIcon && primaryIcon }
-                            type={ primaryType && primaryType }
-                        />
-                    )
-                }
-                
-                {
-                    hasSecondary && (
-                        <SecondaryButton 
-                            text={ secondaryText && secondaryText }
-                            link={ secondaryLink && secondaryLink }
-                            icon={ secondaryIcon && secondaryIcon }
-                            type={ secondaryType && secondaryType }
-                        />
-                    )
-                }
+            <div className={`buttons ${className && className} gap-4 mt-6 flex flex-wrap ${ alignClass && alignClass}`}>
+                { children }
             </div>
         </div>
     )
 }
 
 export default ButtonGroup
+
+// Yeah why not xD
+// How would you do it -
+
+// this is a weird component ngl but i respect it
+//  i feel you could approach it a little differently
+// if you need a button group
+// button group should be a container / wrapper for buttons
+
+// Isn't that what this is? Nah I'm calling the buttons-
+// No unless im confused. This you have the buttons hardcoded.
+
+// i.e <ButtonGroup><PrimaryButton/></ButtonGroup>
+
+// Then that's just making a container for buttons
+
+// I can button group if I want to have buttons xD
+
+// Yes, what else does this do?
+// Look at hero component
+
+
+// const ButtonGroup = ({wrap, alignment, hasPrimary, hasSecondary, primaryText, primaryLink, primaryIcon, primaryType, secondaryText, secondaryLink, secondaryIcon, secondaryType}:ButtonGroupProps) => {
+//     const alignClass = alignment === 'center' 
+//         ? 'justify-center m-auto' : alignment === 'left' 
+//         ? 'justify-start mr-auto' : alignment === 'right' 
+//         ? 'justify-end ml-auto' : null;
+//     return ( 
+//         <div className="button-container">
+//             <div className={`buttons ${wrap} gap-4 mt-6 flex flex-wrap ${ alignClass && alignClass}`}>
+//                 {
+//                     hasPrimary && (
+//                         <PrimaryButton 
+//                             text={ primaryText && primaryText }
+//                             link={ primaryLink && primaryLink }
+//                             icon={ primaryIcon && primaryIcon }
+//                             type={ primaryType && primaryType }
+//                         />
+//                     )
+//                 }
+//                 {
+//                     hasSecondary && (
+//                         <SecondaryButton 
+//                             text={ secondaryText && secondaryText }
+//                             link={ secondaryLink && secondaryLink }
+//                             icon={ secondaryIcon && secondaryIcon }
+//                             type={ secondaryType && secondaryType }
+//                         />
+//                     )
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
