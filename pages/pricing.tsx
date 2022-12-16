38a069c16de39e5be55552ac5820814cad00e40e
Christian <sefluntok@gmail.com>
2022-12-16 15:06:27 +1100

    Fixed pricing button group.

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -3,6 +3,7 @@ import SectionContent from '@components/Section/SectionContent'
 import SectionImage from '@components/Section/SectionImage'
 import Logos from "@components/Logos"
 import ButtonGroup from '@components/ButtonGroup'
+import Button, {ButtonVariant} from '@components/Button/Button'
 
 const Pricing = () => {
     const pageTitle = `Pricing | ${process.env.siteTitle} | Pathway`
@@ -13,20 +14,12 @@ const Pricing = () => {
                 description="Welcome to our website! We offer a wide range of products and services designed to help you achieve your goals. Whether you're looking for information, support, or simply want to learn more, we're here to help. Explore our site to find out more."
             />
             <div className="content-container my-6 sm:my-24">
-                <SectionContent 
-                    title = "Pricing"
-                    alignment = "left"
-                >
+                <SectionContent title = "Pricing" alignment = "left">
                     <p>Fusce ac nulla non urna posuere malesuada eget id ipsum. Praesent vitae sem nisl. Donec in est ut odio sagittis dapibus in commodo mi.</p>
                 </SectionContent>
-                <ButtonGroup 
-                    wrap = "wrap-sm wrap-px"
-                    hasPrimary = {true}
-                    primaryText = "See Prices"
-                    primaryLink = "#prices"
-                    primaryIcon = "ph:arrow-right-bold"
-                    alignment="left"
-                />
+                <ButtonGroup className = "wrap-sm wrap-px" alignment="left">
+                    <Button text="See Prices" link="#prices" icon="ph:arrow-right-bold" variant={ButtonVariant.primary}/>
+                </ButtonGroup>
             </div>
             <SectionImage 
                 className="my-24"
