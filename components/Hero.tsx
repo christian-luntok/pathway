78c41fb0a83cf2baf03050fe736118980ba24809
Christian <sefluntok@gmail.com>
2022-12-16 15:08:08 +1100

    Fixed badge group and button group on hero.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,21 +2,25 @@ import SectionContent from "@components/Section/SectionContent"
 import ButtonGroup from "@components/ButtonGroup"
 import SectionImage from "@components/Section/SectionImage"
 import BadgeGroup from "@components/BadgeGroup"
-
-
+import Button, { ButtonVariant } from "@components/Button/Button"
+import BadgeContext from "@components/Badge/BadgeContext"
+import BadgeMessage from "@components/Badge/BadgeMessage"
+import BadgeIcon from "@components/Badge/BadgeIcon"
 const Hero = () => {
     const alignment = "center"
 
     return (
         <section className="hero-container">
             <div className="hero-content-container py-16 ">
-                <BadgeGroup 
-                    context="V0.1"
-                    message="Welcome to Pathway!"
-                    // icon="ph:arrow-right-bold"
-                    // link="/about"
-                    alignment={alignment}
-                />
+                <BadgeGroup alignment={alignment}>
+                    <BadgeContext>
+                        <span>V0.1</span>
+                    </BadgeContext>
+                    <BadgeMessage>
+                        Welcome to Pathway!
+                    </BadgeMessage>
+                    <BadgeIcon icon="mdi:map-marker-path"/>
+                </BadgeGroup>
                 <SectionContent 
                     title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                     alignment = {alignment}
@@ -24,17 +28,19 @@ const Hero = () => {
                 >
                     <p>Fusce ac nulla non urna posuere malesuada eget id ipsum. Praesent vitae sem nisl. Donec in est ut odio sagittis dapibus in commodo mi.</p>
                 </SectionContent>
-                <ButtonGroup 
-                    wrap = "wrap-sm wrap-px"
-                    hasPrimary = {true}
-                    primaryText = "Get Started"
-                    primaryLink = "/about"
-                    primaryIcon = "ph:arrow-right-bold"
-                    hasSecondary = {true}
-                    secondaryText = "Contact Us"
-                    secondaryLink = "/contact"
-                    alignment={alignment}
-                />
+                <ButtonGroup className = "wrap-sm wrap-px" alignment={alignment}>
+                    <Button 
+                        text="Get Started" 
+                        link="/about"
+                        icon="ph:arrow-right-bold"
+                        variant={ButtonVariant.primary}
+                    />
+                    <Button 
+                        text="Contact Us" 
+                        link="/contact"
+                        variant={ButtonVariant.secondary}
+                    />
+                </ButtonGroup>
             </div>
             <SectionImage 
                 src="/images/Hero.jpg"
