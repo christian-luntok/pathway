ed842c74f155a575b5f1adaea9130e298e9464ac
Christian <sefluntok@gmail.com>
2023-04-12 16:58:50 +1000

    Removed Links pointing to login and signup - Updated to be static.

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,12 +26,12 @@ const Header = () => {
                 <div className="button-container hidden md:flex flex-wrap gap-4 ml-auto">
                     <Button 
                         text="Sign Up"
-                        link="/sign-up"
+                        link="#"
                         variant={ButtonVariant.primary}
                     />
                     <Button 
                         text="Login"
-                        link="/login"
+                        link="#n"
                         variant={ButtonVariant.secondary}
                     />
                 </div>
